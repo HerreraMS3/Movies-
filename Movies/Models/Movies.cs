e5e9247b3f84b7067c7293f522fe4dc2c8c8de00
Alexis Gael Herrera Cambrano <22470069@campeche.tecnm.mx>
2026-04-23 10:50:17 -0600

    Agregar CRUD para las peliculas

---
+++ Movies/Models/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        DateTime ReleaseDate { get; set; }
+        public string? genre { get; set; }
+        public decimal price { get; set; }
+    }
+}
