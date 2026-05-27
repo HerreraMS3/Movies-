fc886987fe1181f14f1a5c2917da67295f4c5e53
Alexis Gael Herrera Cambrano <22470069@campeche.tecnm.mx>
2026-05-27 11:26:46 -0600

    Cambio el idioma de la interfaz de usuario

--- Movies/Models/Movies.cs
+++ Movies/Models/Movies.cs
@@ -5,10 +5,14 @@ namespace Movies.Models
     public class Movie
     {
         public int id { get; set; }
+        [Display(Name ="Titulo")]
         public string? Title { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Liberacion")]
         DateTime ReleaseDate { get; set; }
+        [Display(Name = "Genero")]
         public string? genre { get; set; }
+        [Display(Name = "Precio")]
         public decimal price { get; set; }
     }
 }
